2d2b0c5360a8603ba25b27de3d940a6d6c3a7411
Soham Roy (keybase.io/sohamroy) <soham3.1415@gmail.com>
2019-04-15 02:01:24 -0500

    Main Frontend Progress
    Need to finish backend for further frontend progress.

---
+++ init.js
@@ -0,0 +1,32 @@
+const model = new ChemDoodle.TransformCanvas3D('model', document.getElementById('model').clientWidth, document.getElementById('model').clientHeight);
+model.specs.set3DRepresentation('Ball and Stick');
+model.specs.atoms_sphereDiameter_3D=300;
+model.specs.backgroundColor = '#2196f3';
+model.loadContent();
+
+const sketcher = new ChemDoodle.SketcherCanvas('sketcher', document.getElementById('sketcher').clientWidth, document.getElementById('sketcher').clientHeight,{oneMolecule:true});
+sketcher.specs.atoms_displayTerminalCarbonLabels_2D = true;
+sketcher.specs.atoms_useJMOLColors = true;
+sketcher.specs.bonds_clearOverlaps_2D = true;
+sketcher.specs.bondLength_2D = 75;
+sketcher.specs.atoms_font_size_2D = 25;
+sketcher.repaint();
+
+const updateSpectrum = (jcamp) => {
+	const computed = new ChemDoodle.io.JCAMPInterpreter().makeStructureSpectrumSet('computed', jcamp);
+	computed[0].resize(document.getElementById('computed_molecule').parentElement.clientWidth, document.getElementById('computed_molecule').parentElement.clientHeight);
+	computed[1].resize(document.getElementById('computed_spectrum').parentElement.clientWidth, document.getElementById('computed_spectrum').parentElement.clientHeight);
+	computed[0].specs.atoms_font_size_2D = 12;
+	computed[0].specs.bonds_width_2D = 1.5;
+	//computed[0].specs.scale=2;
+	computed[0].repaint();
+	computed[1].repaint();
+};
+
+updateSpectrum('');
+
+const update = () => {
+	let mol = ChemDoodle.writeMOL(sketcher.getMolecule());
+	let newMol = '';
+	model.loadContent([ChemDoodle.readMOL(newMol,1)]);
+};
